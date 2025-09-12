daa8eebdcfd44bbc2c86ba02f7d1e608d70fc22e
ilyUniv <230006483+IlyUniv@users.noreply.github.com>
2025-09-12 13:01:52 +0300

    STAGE 3: change shell functionality accoridng to the assignment, add more real OS scripts and VFS files

--- shell.py
+++ shell.py
@@ -4,9 +4,78 @@ import socket
 import shlex
 import argparse
 import sys
+from xml.parsers import expat
+from base64 import b64decode
+
+import traceback
+
+class VFS:
+    filesystem = {}
+    using_vfs = False
+    cwd = None
+
+class VFSBuild:
+    cur_dir = None
+    cur_filename = None
+    dir_traceback = []
+
+    def XMLStartElemHandler(name, attr):
+        if name == "rootdir":
+            VFS.filesystem = {}
+            VFSBuild.cur_dir = VFS.filesystem
+            return
+        elif VFSBuild.cur_dir == None:
+            return
+
+        if name == "dir":
+            VFSBuild.cur_dir[attr["name"]] = {}
+            VFSBuild.dir_traceback.append(VFSBuild.cur_dir)
+            VFSBuild.cur_dir = VFSBuild.cur_dir[attr["name"]]
+        elif name == "file" and VFSBuild.cur_filename == None:
+            VFSBuild.cur_dir[attr["name"]] = ""
+            VFSBuild.cur_filename = attr["name"]
+    
+    def XMLChDataHandler(data):
+        if VFSBuild.cur_filename:
+            VFSBuild.cur_dir[VFSBuild.cur_filename] = data
+    
+    def XMLEndElemHandler(name):
+        if name == "dir":
+            VFSBuild.cur_dir = VFSBuild.dir_traceback.pop()
+        elif name == "file":
+            VFSBuild.cur_filename = None
+        elif name == "rootdir":
+            VFSBuild.cur_dir = None
+
 
 def getrelativepathstr(path: str):
     return path.replace(os.path.expanduser("~"), "~")
+
+def buildvfs(path: str):
+    VFS.using_vfs = True
+
+    parser = expat.ParserCreate()
+
+    parser.StartElementHandler = VFSBuild.XMLStartElemHandler
+    parser.EndElementHandler = VFSBuild.XMLEndElemHandler
+    parser.CharacterDataHandler = VFSBuild.XMLChDataHandler
+
+    try:
+        with open(path, 'rb') as f:
+            head = f.read(5)
+            f.seek(0, os.SEEK_SET)
+
+            if head == b'<?xml':
+                
+                parser.ParseFile(f)
+            else:
+                b64 = f.read()
+                s = b64decode(b64)
+                parser.Parse(s)
+    except Exception:
+        print("[VFS!!!] ERROR:", traceback.format_exc())
+    
+    print("[VFS] Built:", VFS.filesystem)
 
 def processcommand(cmd: str):
     try:
@@ -74,6 +143,9 @@ def main():
     print("VFS:", vfs)
     print("Start Script:", startscript)
     print("--------------------------------------\n")
+
+    if vfs:
+        buildvfs(vfs)
 
     if startscript:
         execscript(startscript)
---
+++ start_vfstest_fewfiles.sh
@@ -0,0 +1,1 @@
+python3 $(dirname $0)/shell.py --vfs=$(dirname $0)/vfstest/1_fewfiles.xml
